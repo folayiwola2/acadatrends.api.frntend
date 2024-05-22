0ce2f4fa734c7a49b9bc4f2831b6b8ba75abb87b
Olufemi Olayiwola <94996298+folayiwola2@users.noreply.github.com>
2024-05-22 20:31:37 +0100

    client,build

---
+++ precache-manifest.ac6b47b38971a9a6c726821c5daa3c7c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "25fef3f99ce3e078b7b0",
+    "url": "/static/css/main.42514adc.chunk.css"
+  },
+  {
+    "revision": "25fef3f99ce3e078b7b0",
+    "url": "/static/js/main.25fef3f9.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7d9a9341df2162d38e98",
+    "url": "/static/js/2.7d9a9341.chunk.js"
+  },
+  {
+    "revision": "5300322f5a86f895c106cee849dbffaa",
+    "url": "/static/media/logo-acada-black.5300322f.svg"
+  },
+  {
+    "revision": "7c4a462727662906c5509ff9036473e3",
+    "url": "/static/media/image1.7c4a4627.jpg"
+  },
+  {
+    "revision": "033c1b8bd828a3364cc4e4e308e71011",
+    "url": "/static/media/image2.033c1b8b.jpg"
+  },
+  {
+    "revision": "c21573f13e3564723ab72ce4d4cf0dfb",
+    "url": "/index.html"
+  }
+];
